1d82bbe3e00f73090378b301b466e7fd91970de4
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-04-22 12:08:04 +0100

    emulator removed

--- messages/index.js
+++ messages/index.js
@@ -45,7 +45,7 @@ var callback = function(token){
 // });
 
 var useEmulator = (process.env.NODE_ENV == 'development');
-useEmulator = true;
+//useEmulator = true;
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
     appId: process.env['MicrosoftAppId'],
